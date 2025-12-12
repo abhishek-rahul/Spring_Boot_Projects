5c12c6e967d294e0e8e09818ebb38212bd17714f
abhishek <abhishek37514@gmail.com>
2025-12-12 18:14:06 +0530

    added userservice

--- user-service/src/main/java/com/abhicom/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/abhicom/user_service/UserServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+@Configuration
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
